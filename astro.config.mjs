979b0313e47e5cf778bdc26cef61a9b941a44468
Lemasc Service <lemascserviceth@hotmail.com>
2023-04-11 18:27:58 +0700

    Bootstrap project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+  output: "server",
+  adapter: vercel(),
+});
