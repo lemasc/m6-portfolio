4ef9e3ed7798826fde430c2e2640d28cbaf9b99e
Lemasc Service <lemascserviceth@hotmail.com>
2023-04-16 11:40:12 +0700

    WIP: Landing page animation

---
+++ src/sections/Landing/animate.ts
@@ -0,0 +1,32 @@
+import anime from "animejs";
+export const animate = (parent: HTMLElement | null) => {
+  if (!parent) return;
+  const timeline = anime.timeline({
+    autoplay: false,
+  });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      console.log("OBSERVER", entries);
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          parent.style.opacity = "1";
+          for (let i = 0; i < parent.children.length; i++) {
+            timeline.add({
+              targets: parent.children[i],
+              opacity: [0, 1],
+              duration: 500,
+              easing: "easeInOutQuad",
+              delay: 300,
+            });
+          }
+          timeline.play();
+          observer.unobserve(parent);
+        }
+      });
+    },
+    {
+      root: document.querySelector("main"),
+    }
+  );
+  observer.observe(parent);
+};
